6c085eb8f5a81f25edff057d497df947ef421f73
cyjico <37214242+cyjico@users.noreply.github.com>
2024-11-27 15:56:45 +0800

    refactor: update main() to initialize and run Application

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+#include "Application.h"
+#include "pages/IndexPage.h"
 #include <iostream>
+#include <memory>
 
-int main(int, char **) { std::cout << "Build is working!\n"; }
+int main() {
+  const PageMap pages = {{"/", std::make_shared<IndexPage>(IndexPage())}};
+
+  Application app(pages);
+  app.RunIndefinitely();
+
+  std::cout << "Reached end of program.";
+  return 0;
+}
